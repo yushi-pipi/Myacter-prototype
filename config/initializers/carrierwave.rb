da30ba2f94992bcc59f13cb46a99678bc7adf1a5
池田侑史 <ikedayuushi@ikedayuushinoMacBook-Air.local>
2020-02-04 19:30:38 +0900

    create imageuploader2

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: ENV['S3_REGION'],
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory = ENV['myacterstrage']
+  end
+end

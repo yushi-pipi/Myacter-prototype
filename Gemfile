a788f4f44bb2766956ce1a6476db816474d08a6c
池田侑史 <ikedayuushi@ikedayuushinoMacBook-Air.local>
2020-02-04 23:30:36 +0900

    aws-Adaptation

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'carrierwave',             '1.2.2'
 gem 'mini_magick',             '4.7.0'
 gem 'will_paginate',           '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
-gem 'fog-aws'
+gem 'fog'
 
 group :development, :test do
   gem 'byebug',  '9.0.6', platform: :mri
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = 'fog'
     config.fog_credentials = {
       provider: 'AWS',
       region: ENV['ap-northeast-1'],
-      aws_access_key_id: ENV[S3_ACCESS_KEY],
-      aws_secret_access_key: ENV[S3_SECRET_KEY]
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
     config.fog_directory = ENV['myacterstrage']
   end

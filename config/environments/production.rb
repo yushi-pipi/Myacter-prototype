d766cc5998f837550dd4780d9bd90e6f6661f15a
池田侑史 <ikedayuushi@ikedayuushinoMacBook-Air.local>
2020-02-04 15:20:43 +0900

    Add user Activities

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://rails-tutorial-sample-yushi.herokuapp.com'
+  host = 'https://myacter.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',

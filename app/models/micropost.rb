e2a04d47470cc1c5086b42f40123330ea9fb597b
yukino <ikedayuushi@ikedayuushinoMacBook-Air.local>
2020-03-13 09:19:22 +0900

    chart-add-options-range

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@
 
 class Micropost < ApplicationRecord
   belongs_to :activity
-  belongs_to :user
+  # belongs_to :user
   # default_scope -> { order(created_at: :desc) }
   validates :activity_id, presence: true
 end
